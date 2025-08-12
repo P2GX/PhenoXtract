7e19edc3bceb1b9dfd156b68afab13a17b080985
rouvenreuter <rouven.reuter91@gmail.com>
2025-08-12 14:25:33 +0200

    Use phenopacket crate

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,4 @@
-mod config;
-mod extract;
-mod load;
-mod pipeline;
-mod transform;
 fn main() {
-    use phenopackets::schema::v2::Phenopacket;
-    let a = Phenopacket::default();
     println!("Hello, world! ✌️");
 }
 
