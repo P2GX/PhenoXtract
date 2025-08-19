5bd455857422037419b0cbb8825be775594e2d58
rouvenreuter <rouven.reuter91@gmail.com>
2025-08-19 09:13:14 +0200

    ContextualizedDataFrame: Add new function

--- src/extract/contextualized_data_frame.rs
+++ src/extract/contextualized_data_frame.rs
@@ -1,13 +1,24 @@
 use crate::config::table_context::TableContext;
-use polars::prelude::*;
+use polars::prelude::DataFrame;
 
 /// A structure that combines a `DataFrame` with its corresponding `TableContext`.
 ///
 /// This allows for processing the data within the `DataFrame` according to the
 /// rules and semantic information defined in the context.
+#[derive(Debug, PartialEq)]
 pub struct ContextualizedDataFrame {
     #[allow(unused)]
     context: TableContext,
     #[allow(unused)]
     data: DataFrame,
+}
+
+impl ContextualizedDataFrame {
+    pub fn new(context: TableContext, data: DataFrame) -> Self {
+        ContextualizedDataFrame { context, data }
+    }
+
+    pub fn context(&self) -> &TableContext {
+        &self.context
+    }
 }
