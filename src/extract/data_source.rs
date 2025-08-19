51b7ea54de0e3d15260a8fe97d2862f3f3537b4c
rouvenreuter <rouven.reuter91@gmail.com>
2025-08-19 09:17:04 +0200

    DataSource: Add test for csv extraction

--- src/extract/data_source.rs
+++ src/extract/data_source.rs
@@ -1,5 +1,7 @@
 use crate::extract::contextualized_data_frame::ContextualizedDataFrame;
 use crate::extract::csv_data_source::CSVDataSource;
+use polars::io::SerReader;
+use polars::prelude::CsvReadOptions;
 
 use crate::extract::excel_data_source::ExcelDatasource;
 use crate::extract::traits::Extractable;
@@ -35,5 +37,60 @@ impl Extractable for DataSource {
                 todo!("Excel extraction is not yet implemented.")
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::config::table_context::{
+        CellContext, Context, Identifier, SeriesContext, SingleSeriesContext, TableContext,
+    };
+    use rstest::{fixture, rstest};
+    use std::fs::File;
+    use std::io::Write;
+    use tempfile::TempDir;
+
+    const CSV_DATA: &[u8] = br#"patient_id,hpo_id,disease_id,sex
+                                P001,HP:0000505,MONDO:100100,M
+                                P002,HP:0000252,MONDO:100200,F
+                                P003,HP:0001250,MONDO:100300,M
+                                P004,HP:0000768,MONDO:100400,F"#;
+
+    #[fixture]
+    fn temp_dir() -> TempDir {
+        tempfile::tempdir().expect("Failed to create temporary directory")
+    }
+
+    #[rstest]
+    fn test_load_csv(temp_dir: TempDir) {
+        let file_path = temp_dir.path().join("csv_data.csv");
+        let mut file = File::create(&file_path).unwrap();
+        file.write_all(CSV_DATA).unwrap();
+
+        let table_context = TableContext::new(
+            "test_table".to_string(),
+            vec![SeriesContext::Single(SingleSeriesContext::new(
+                Identifier::Name("patient_id".to_string()),
+                Context::None,
+                Some(CellContext::new(
+                    Context::SubjectId,
+                    None,
+                    Default::default(),
+                )),
+                Some("Link_A".to_string()),
+                vec!["HP:0000054".to_string()],
+            ))],
+            vec![],
+        );
+
+        let data_source =
+            DataSource::Csv(CSVDataSource::new(file_path, None, table_context.clone()));
+
+        let mut data_frames = data_source.extract().unwrap();
+        let context_df = data_frames.pop().unwrap();
+
+        assert_eq!(context_df.context(), &table_context);
+        dbg!(&data_frames);
     }
 }
