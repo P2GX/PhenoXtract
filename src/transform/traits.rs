7139e095c594fbceec8d3490d9b283d2344a7313
psnairne <patricknairne@hotmail.co.uk>
2025-08-27 15:31:45 +0200

    a first attempt at creating a simple transform struct

--- src/transform/traits.rs
+++ src/transform/traits.rs
@@ -1,5 +1,7 @@
+use crate::config::table_context::Context;
 use crate::extract::contextualized_data_frame::ContextualizedDataFrame;
 use crate::transform::error::TransformError;
+use polars::datatypes::DataType;
 
 #[allow(dead_code)]
 /// Represents a strategy for transforming a `ContextualizedDataFrame`.
@@ -25,4 +27,87 @@ pub trait Strategy {
 
     fn internal_transform(&self, table: &mut ContextualizedDataFrame)
     -> Result<(), TransformError>;
+}
+
+struct WordSwap {
+    input_string: String,
+    output_string: String,
+    expected_context: Option<Context>,
+}
+
+impl Strategy for WordSwap {
+    fn is_valid(&self, table: &ContextualizedDataFrame) -> bool {
+        let only_string_cols_bool = table
+            .data()
+            .dtypes()
+            .iter()
+            .all(|dtype| *dtype == DataType::String);
+        let expected_context_bool = match &self.expected_context {
+            Some(context) => table
+                .context()
+                .context
+                .iter()
+                .all(|s_context| *context == s_context.get_context()),
+            None => true,
+        };
+        only_string_cols_bool && expected_context_bool
+    }
+    fn internal_transform(
+        &self,
+        table: &mut ContextualizedDataFrame,
+    ) -> Result<(), TransformError> {
+        //how to do this...
+
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::config::table_context::{
+        CellContext, Context, SeriesContext, SingleSeriesContext, TableContext,
+    };
+    use crate::extract::contextualized_data_frame::ContextualizedDataFrame;
+    use crate::transform::traits::{Strategy, WordSwap};
+    use polars::frame::DataFrame;
+    use polars::prelude::Column;
+    use rstest::{fixture, rstest};
+
+    #[fixture]
+    fn tc() -> TableContext {
+        TableContext::new(
+            "patient_sex_column".to_string(),
+            vec![SeriesContext::Single(SingleSeriesContext::new(
+                "sex".to_string(),
+                Context::SubjectSex,
+                Some(CellContext::new(
+                    Context::SubjectId,
+                    None,
+                    Default::default(),
+                )),
+                vec![],
+            ))],
+        )
+    }
+
+    #[fixture]
+    fn data() -> DataFrame {
+        let col = Column::new("sex".into(), ["Male", "Female", "Female", "Male"]);
+        DataFrame::new(vec![col]).unwrap()
+    }
+
+    #[fixture]
+    fn cdf(tc: TableContext, data: DataFrame) -> ContextualizedDataFrame {
+        ContextualizedDataFrame::new(tc, data)
+    }
+
+    #[rstest]
+    fn test_transformation(mut cdf: ContextualizedDataFrame) {
+        let a = WordSwap {
+            input_string: String::from("Male"),
+            output_string: String::from("M"),
+            expected_context: Some(Context::SubjectSex),
+        };
+        a.transform(&mut cdf).expect("transform failed");
+    }
 }
