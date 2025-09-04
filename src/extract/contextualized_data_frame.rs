a5e9b52987c619a846a3533415061d34c75db1f5
rouvenreuter <rouven.reuter91@gmail.com>
2025-09-04 09:30:16 +0200

    ContextualizedDataFrame: Add utility functions

--- src/extract/contextualized_data_frame.rs
+++ src/extract/contextualized_data_frame.rs
@@ -29,13 +29,43 @@ impl ContextualizedDataFrame {
         &mut self.context
     }
 
-    #[allow(unused)]
-    pub fn data(&self) -> &DataFrame {
-        &self.data
+    pub fn replace_context_id(&mut self, old: &str, new: &str) -> Result<(), anyhow::Error> {
+        let series_context = self.get_series_context_mut(old);
+        let series_context_unwrapped =
+            series_context.ok_or_else(|| anyhow!("No context found for identifier {old}."))?;
+
+        match series_context_unwrapped {
+            SeriesContext::Single(_) => {
+                series_context_unwrapped.set_id(SetId::Single(new.to_owned()))?;
+            }
+            SeriesContext::Multi(multi) => {
+                if let MultiIdentifier::Multi(ref mut ids) = multi.multi_identifier {
+                    if let Some(index) = ids.iter().position(|word| word == old) {
+                        ids[index] = new.to_string();
+                    } else {
+                        return Err(anyhow!("Could not replace multi id: {old} not found."));
+                    }
+                } else if let MultiIdentifier::Regex(_) = multi.multi_identifier {
+                    series_context_unwrapped.set_id(SetId::MultiRegex(new.to_owned()))?;
+                }
+            }
+        }
+
+        Ok(())
     }
 
     #[allow(unused)]
-    pub fn data_mut(&mut self) -> &mut DataFrame {
-        &mut self.data
+    pub fn get_series_context(&self, identifier: &str) -> Option<&SeriesContext> {
+        self.context
+            .context
+            .iter()
+            .find(|ctx| ctx.matches_identifier(identifier))
+    }
+
+    pub fn get_series_context_mut(&mut self, identifier: &str) -> Option<&mut SeriesContext> {
+        self.context
+            .context
+            .iter_mut()
+            .find(|ctx| ctx.matches_identifier(identifier))
     }
 }
