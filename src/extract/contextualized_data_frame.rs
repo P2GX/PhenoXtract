96ddddf94db9539d549ebc04a293007ea68099d8
rouvenreuter <rouven.reuter91@gmail.com>
2025-09-10 13:57:35 +0200

    Add errors

--- src/extract/contextualized_data_frame.rs
+++ src/extract/contextualized_data_frame.rs
@@ -1,11 +1,12 @@
 use crate::config::table_context::TableContext;
 use polars::prelude::DataFrame;
+use validator::Validate;
 
 /// A structure that combines a `DataFrame` with its corresponding `TableContext`.
 ///
 /// This allows for processing the data within the `DataFrame` according to the
 /// rules and semantic information defined in the context.
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Validate)]
 pub struct ContextualizedDataFrame {
     #[allow(unused)]
     context: TableContext,
--- src/extract/excel_data_source.rs
+++ src/extract/excel_data_source.rs
@@ -2,13 +2,20 @@ use crate::config::table_context::TableContext;
 
 use crate::extract::extraction_config::ExtractionConfig;
 use crate::extract::traits::HasSource;
-use crate::validation::data_source_validation::validate_unique_sheet_names;
+use crate::validation::data_source_validation::{
+    validate_extraction_config_links, validate_extraction_config_unique_ids,
+    validate_unique_sheet_names,
+};
 use serde::{Deserialize, Serialize};
 use std::path::PathBuf;
 use validator::Validate;
 
 /// Defines an Excel workbook as a data source.
 #[derive(Debug, Validate, Deserialize, Serialize, Clone)]
+#[validate(schema(
+    function = "validate_extraction_config_links",
+    skip_on_field_errors = false
+))]
 pub struct ExcelDatasource {
     /// The file path to the Excel workbook.
     #[allow(unused)]
@@ -17,7 +24,9 @@ pub struct ExcelDatasource {
     #[allow(unused)]
     #[validate(custom(function = "validate_unique_sheet_names"))]
     pub contexts: Vec<TableContext>,
+
     /// One extraction config for every worksheet from the workbook that will be extracted.
+    #[validate(custom(function = "validate_extraction_config_unique_ids"))]
     pub extraction_configs: Vec<ExtractionConfig>,
 }
 
