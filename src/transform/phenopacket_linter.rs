a60c9e3c7f7456921943749008c81bd27c4d5c92
rouvenreuter <rouven.reuter91@gmail.com>
2025-09-23 21:33:15 +0200

    refactor find_related_phenotypic_features

--- src/transform/phenopacket_linter.rs
+++ src/transform/phenopacket_linter.rs
@@ -1,11 +1,14 @@
+// TODO: Remove when done
+#![allow(dead_code)]
+#![allow(unused)]
 use log::debug;
+use ontolius::TermId;
 use ontolius::ontology::HierarchyQueries;
 use ontolius::ontology::csr::FullCsrOntology;
-use ontolius::{Identified, TermId};
 use phenopackets::schema::v2::Phenopacket;
 use phenopackets::schema::v2::core::OntologyClass;
 use phenopackets::schema::v2::core::PhenotypicFeature;
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::rc::Rc;
 use std::str::FromStr;
 
@@ -78,52 +81,68 @@ impl PhenopacketLinter {
             }
         }
 
-        let mut invalid_terms = HashSet::new();
-
+        // Case 1: Invalidate all ancestors of a family for an observed term
         // Amongst the observed terms, we want to keep the most specific ones.
         // Which means, if we find a term that is more general then another, we deem the more general term invalid.
-        // If there is a more specific excluded term, we should invalidate that as well.
-        // In this case we assume that the excluded term is invalid,  because a specific ancestor was annotated
-        for observed_term in &observed {
-            for other_observed in &observed {
-                if observed_term != other_observed
-                    && self.hpo.is_ancestor_of(observed_term, other_observed)
-                {
-                    debug!(
-                        "Found related terms amongst observed terms {:?} -> {:?}",
-                        observed_term.identifier().to_string(),
-                        other_observed.identifier().to_string()
-                    );
-                    invalid_terms.insert(observed_term.clone());
-                }
-            }
-            for excluded_term in &excluded {
-                debug!(
-                    "Found related terms amongst observed and excluded terms {:?} -> {:?}",
-                    observed_term.identifier().to_string(),
-                    excluded_term.identifier().to_string()
-                );
-                if self.hpo.is_ancestor_of(observed_term, excluded_term) {
-                    invalid_terms.insert(excluded_term.clone());
-                }
-            }
-        }
+        let invalid_observed_ancestors = observed
+            .iter()
+            .flat_map(|term| self.find_ancestors(&observed, term))
+            .collect::<HashSet<TermId>>();
 
-        // Amongst the excluded terms, we want to keep the most general.
+        debug!(
+            "Found invalid observed ancestors: {:?}",
+            invalid_observed_ancestors
+        );
+
+        // Case 2: Invalidate excluded terms that share the same family with an observed term and are descendents
+        // If there is a more specific excluded term, we should invalidate that as well.
+        // In this case we assume that the excluded term is invalid, because a specific ancestor was annotated
+        let invalid_excluded_observed_descendents = observed
+            .iter()
+            .flat_map(|term| self.find_descendents(&excluded, term))
+            .collect::<HashSet<TermId>>();
+
+        debug!(
+            "Found invalid excluded/observed descendents: {:?}",
+            invalid_excluded_observed_descendents
+        );
+
+        // Case 1: Invalidate all descendents of a family for an excluded term
         // Because, if you can exclude a general phenotype the specific one can also be excluded.
-        for excluded_term in &excluded {
-            for other in &excluded {
-                if excluded_term != other && self.hpo.is_descendant_of(excluded_term, other) {
-                    debug!(
-                        "Found related terms amongst excluded terms {:?} -> {:?}",
-                        excluded_term.identifier().to_string(),
-                        other.identifier().to_string()
-                    );
-                    invalid_terms.insert(other.clone());
-                }
-            }
-        }
-        invalid_terms.iter().map(ToString::to_string).collect()
+        let invalid_excluded_descendents = excluded
+            .iter()
+            .flat_map(|term| self.find_descendents(&excluded, term))
+            .collect::<HashSet<TermId>>();
+
+        debug!(
+            "Found invalid excluded descendents: {:?}",
+            invalid_excluded_descendents
+        );
+
+        invalid_observed_ancestors
+            .into_iter()
+            .chain(invalid_excluded_observed_descendents)
+            .chain(invalid_excluded_descendents)
+            .collect::<HashSet<TermId>>()
+            .iter()
+            .map(ToString::to_string)
+            .collect()
+    }
+
+    fn find_ancestors(&self, ancestry: &HashSet<TermId>, scion: &TermId) -> HashSet<TermId> {
+        ancestry
+            .iter()
+            .filter(|term| *term != scion && self.hpo.is_ancestor_of(*term, scion))
+            .cloned()
+            .collect()
+    }
+
+    fn find_descendents(&self, ancestry: &HashSet<TermId>, progenitor: &TermId) -> HashSet<TermId> {
+        ancestry
+            .iter()
+            .filter(|term| *term != progenitor && self.hpo.is_descendant_of(*term, progenitor))
+            .cloned()
+            .collect()
     }
 
     fn ontology_class_to_term_id(ontology_class: &OntologyClass) -> TermId {
@@ -147,11 +166,20 @@ mod tests {
         TempDir::new().unwrap()
     }
 
+    #[fixture]
+    fn term_ancestry() -> Vec<TermId> {
+        vec![
+            "HP:0000448".parse().unwrap(), // scion
+            "HP:0005105".parse().unwrap(),
+            "HP:0000366".parse().unwrap(),
+            "HP:0000271".parse().unwrap(), // progenitor
+        ]
+    }
+
     fn construct_linter(tmp_dir: TempDir) -> PhenopacketLinter {
-        let hpo_registry = GithubOntologyRegistry::default_hpo_registry()
-            .unwrap()
-            .with_registry_path(tmp_dir.path().into());
-        let path = hpo_registry.register("latest").unwrap();
+        let hpo_registry = GithubOntologyRegistry::default_hpo_registry().unwrap();
+        //.with_registry_path(tmp_dir.path().into());
+        let path = hpo_registry.register("v2025-09-01").unwrap();
 
         PhenopacketLinter {
             hpo: init_ontolius(path).unwrap(),
@@ -159,17 +187,38 @@ mod tests {
     }
 
     #[rstest]
-    fn test_find_related_phenotypic_features_exclude_general_term(tmp_dir: TempDir) {
+    fn test_find_ancestors(tmp_dir: TempDir, term_ancestry: Vec<TermId>) {
+        skip_in_ci!();
+        let linter = construct_linter(tmp_dir);
+
+        let ancestors = linter.find_ancestors(
+            &term_ancestry.iter().cloned().collect(),
+            &"HP:0005105".parse().unwrap(),
+        );
+
+        assert!(ancestors.contains(&TermId::from_str("HP:0000366").unwrap()));
+        assert!(ancestors.contains(&TermId::from_str("HP:0000271").unwrap()));
+    }
+
+    #[rstest]
+    fn test_find_descendents(tmp_dir: TempDir, term_ancestry: Vec<TermId>) {
+        skip_in_ci!();
+        let linter = construct_linter(tmp_dir);
+
+        let ancestors = linter.find_descendents(
+            &term_ancestry.iter().cloned().collect(),
+            &"HP:0005105".parse().unwrap(),
+        );
+
+        assert!(ancestors.contains(&TermId::from_str("HP:0000448").unwrap()));
+    }
+
+    #[rstest]
+    fn test_find_related_phenotypic_features_case_1(tmp_dir: TempDir) {
+        skip_in_ci!();
         let linter = construct_linter(tmp_dir);
 
         let phenotypic_features = vec![
-            PhenotypicFeature {
-                r#type: Some(OntologyClass {
-                    id: "HP:0000448".to_string(),
-                    label: "Prominent nose".to_string(),
-                }),
-                ..Default::default()
-            },
             PhenotypicFeature {
                 r#type: Some(OntologyClass {
                     id: "HP:0005105".to_string(),
@@ -177,45 +226,31 @@ mod tests {
                 }),
                 ..Default::default()
             },
-        ];
-
-        let invalid_terms = linter.find_related_phenotypic_features(&phenotypic_features);
-        if let Some(hpo_id) = invalid_terms.iter().next() {
-            assert_eq!(hpo_id, "HP:0005105");
-        }
-    }
-
-    #[rstest]
-    fn test_find_related_phenotypic_features_exclude_specific_excluded_term(tmp_dir: TempDir) {
-        let linter = construct_linter(tmp_dir);
-
-        let phenotypic_features = vec![
             PhenotypicFeature {
                 r#type: Some(OntologyClass {
-                    id: "HP:0000608".to_string(),
-                    label: "Macular degeneration".to_string(),
+                    id: "HP:0000366".to_string(),
+                    label: "Abnormality of the nose".to_string(),
                 }),
-                excluded: true,
                 ..Default::default()
             },
             PhenotypicFeature {
                 r#type: Some(OntologyClass {
-                    id: "HP:0001098".to_string(),
-                    label: "Abnormal fundus morphology".to_string(),
+                    id: "HP:0000448".to_string(),
+                    label: "Prominent nose".to_string(),
                 }),
-                excluded: true,
                 ..Default::default()
             },
         ];
 
         let invalid_terms = linter.find_related_phenotypic_features(&phenotypic_features);
-        if let Some(hpo_id) = invalid_terms.iter().next() {
-            assert_eq!(hpo_id, "HP:0001098");
-        }
+        assert_eq!(invalid_terms.len(), 2);
+        invalid_terms.contains("HP:0005105");
+        invalid_terms.contains("HP:0000366");
     }
 
     #[rstest]
-    fn test_find_related_phenotypic_features_excluded_more_specific(tmp_dir: TempDir) {
+    fn test_find_related_phenotypic_features_case_2(tmp_dir: TempDir) {
+        skip_in_ci!();
         let linter = construct_linter(tmp_dir);
 
         let phenotypic_features = vec![
@@ -238,9 +273,39 @@ mod tests {
         ];
 
         let invalid_terms = linter.find_related_phenotypic_features(&phenotypic_features);
+        assert_eq!(invalid_terms.len(), 1);
         if let Some(hpo_id) = invalid_terms.iter().next() {
             assert_eq!(hpo_id, "HP:0000608");
         }
+    }
+
+    #[rstest]
+    fn test_find_related_phenotypic_features_case_3(tmp_dir: TempDir) {
+        skip_in_ci!();
+        let linter = construct_linter(tmp_dir);
+
+        let phenotypic_features = vec![
+            PhenotypicFeature {
+                r#type: Some(OntologyClass {
+                    id: "HP:0001098".to_string(),
+                    label: "Abnormal fundus morphology".to_string(),
+                }),
+                excluded: true,
+                ..Default::default()
+            },
+            PhenotypicFeature {
+                r#type: Some(OntologyClass {
+                    id: "HP:0000608".to_string(),
+                    label: "Macular degeneration".to_string(),
+                }),
+                excluded: true,
+                ..Default::default()
+            },
+        ];
+
+        let invalid_terms = linter.find_related_phenotypic_features(&phenotypic_features);
+        assert_eq!(invalid_terms.len(), 1);
+        invalid_terms.contains("HP:0001098");
     }
 
     #[rstest]
