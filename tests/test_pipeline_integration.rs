c119eafa91a7c534acf9afe27f3e26be1a833e8c
psnairne <patricknairne@hotmail.co.uk>
2025-09-29 14:26:34 +0200

    first version of integration test.
    Main difference is that the example test data is slightly more realistic, and incorporates age of onset and Regex

--- tests/test_pipeline_integration.rs
+++ tests/test_pipeline_integration.rs
@@ -1,4 +1,3 @@
-use phenopackets::schema::v2::Phenopacket;
 use phenoxtract::Pipeline;
 use phenoxtract::config::table_context::{
     AliasMap, Context, Identifier, SeriesContext, TableContext,
@@ -15,29 +14,187 @@ use phenoxtract::transform::strategies::{
 };
 use phenoxtract::transform::traits::Strategy;
 use phenoxtract::transform::{Collector, PhenopacketBuilder, TransformerModule};
-use rstest::rstest;
+use rstest::{fixture, rstest};
 use std::collections::HashMap;
 use std::fs;
 use std::path::PathBuf;
+use phenopackets::schema::v2::Phenopacket;
+
+#[fixture]
+fn vital_status_aliases() -> AliasMap {
+    let mut vs_alias_map: HashMap<String, String> = HashMap::default();
+    vs_alias_map.insert("Yes".to_string(), "ALIVE".to_string());
+    vs_alias_map.insert("No".to_string(), "DECEASED".to_string());
+    AliasMap::ToString(vs_alias_map)
+}
+
+#[fixture]
+fn csv_context() -> TableContext {
+    TableContext::new(
+        "CSV_Table".to_string(),
+        vec![
+            SeriesContext::new(
+                Identifier::Regex("0".to_string()),
+                Default::default(),
+                Context::SubjectId,
+                None,
+                None,
+                vec![],
+            ),
+            SeriesContext::new(
+                Identifier::Regex("1".to_string()),
+                Default::default(),
+                Context::HpoLabel,
+                None,
+                None,
+                vec![],
+            ),
+            SeriesContext::new(
+                Identifier::Regex("2".to_string()),
+                Default::default(),
+                Context::HpoLabel,
+                None,
+                None,
+                vec![],
+            ),
+        ],
+    )
+}
+
+#[fixture]
+fn excel_context(vital_status_aliases: AliasMap) -> Vec<TableContext> {
+    vec![
+        TableContext::new(
+            "basic info".to_string(),
+            vec![
+                SeriesContext::new(
+                    Identifier::Regex("Patient ID".to_string()),
+                    Default::default(),
+                    Context::SubjectId,
+                    None,
+                    None,
+                    vec![],
+                ),
+                SeriesContext::new(
+                    Identifier::Regex("Sex".to_string()),
+                    Default::default(),
+                    Context::SubjectSex,
+                    None,
+                    None,
+                    vec![],
+                ),
+                SeriesContext::new(
+                    Identifier::Regex("Living".to_string()),
+                    Default::default(),
+                    Context::VitalStat,
+                    None,
+                    Some(vital_status_aliases),
+                    vec![],
+                ),
+            ],
+        ),
+        TableContext::new(
+            "conditions".to_string(),
+            vec![
+                SeriesContext::new(
+                    Identifier::Regex("Patient ID".to_string()),
+                    Default::default(),
+                    Context::SubjectId,
+                    None,
+                    None,
+                    vec![],
+                ),
+                SeriesContext::new(
+                    Identifier::Regex("Phenotypic Features".to_string()),
+                    Default::default(),
+                    Context::HpoLabel,
+                    None,
+                    None,
+                    vec![Identifier::Regex("Age of onset".to_string())],
+                ),
+                SeriesContext::new(
+                    Identifier::Regex("Age of onset".to_string()),
+                    Default::default(),
+                    Context::OnsetAge,
+                    None,
+                    None,
+                    vec![],
+                ),
+            ],
+        ),
+        TableContext::new(
+            "more conditions".to_string(),
+            vec![
+                SeriesContext::new(
+                    Identifier::Regex("Patient ID".to_string()),
+                    Default::default(),
+                    Context::SubjectId,
+                    None,
+                    None,
+                    vec![],
+                ),
+                SeriesContext::new(
+                    Identifier::Regex(r"Phenotypic Features \d+".to_string()),
+                    Default::default(),
+                    Context::HpoLabel,
+                    None,
+                    None,
+                    vec![],
+                ),
+            ],
+        ),
+    ]
+}
 
 #[rstest]
-pub fn test_pipeline_integration() {
+fn test_pipeline_integration(csv_context: TableContext, excel_context: Vec<TableContext>) {
     // TODO: Load pipeline from config file. Can not be done yet, because strategies are not ready to be loaded from file
-    let cohort_name = "test_cohort";
+
+    //Set-up
+    let cohort_name = "my_cohort";
     let hpo_registry = GithubOntologyRegistry::default_hpo_registry().unwrap();
     let hpo = init_ontolius(hpo_registry.register("v2025-09-01").unwrap()).unwrap();
+    let assets_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join(
+        PathBuf::from(file!())
+            .parent()
+            .unwrap()
+            .join("assets"),
+    );
 
+    //Configure data sources and contexts
+    let csv_path = assets_path.clone().join("csv_data.csv");
+    let excel_path = assets_path.clone().join("excel_data.xlsx");
+
+    let mut data_sources = [
+        DataSource::Csv(CSVDataSource::new(
+            csv_path,
+            None,
+            csv_context,
+            ExtractionConfig::new("CSV_Table".to_string(), false, true),
+        )),
+        DataSource::Excel(ExcelDatasource::new(
+            excel_path,
+            excel_context,
+            vec![
+                ExtractionConfig::new("basic info".to_string(), true, true),
+                ExtractionConfig::new("conditions".to_string(), true, false),
+                ExtractionConfig::new("more conditions".to_string(), true, false),
+            ],
+        )),
+    ];
+
+    //Configure strategies (a.k.a. transformations)
     let strategies: Vec<Box<dyn Strategy>> = vec![
         Box::new(AliasMapStrategy),
         Box::new(HPOSynonymsToPrimaryTermsStrategy::new(hpo.clone())),
         Box::new(SexMappingStrategy::default()),
     ];
 
-    let manifest_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-    let assets_path = manifest_path.join(PathBuf::from(file!()).parent().unwrap().join("assets"));
-
-    let collector = Collector::new(PhenopacketBuilder::new(hpo), cohort_name.to_owned());
-    let tm = TransformerModule::new(strategies, collector);
+    //Create the pipeline
+    let transformer_module = TransformerModule::new(
+        strategies,
+        Collector::new(PhenopacketBuilder::new(hpo), cohort_name.to_owned()),
+    );
 
     let output_dir = assets_path.join("do_not_push");
     if !output_dir.exists() {
@@ -45,93 +202,28 @@ pub fn test_pipeline_integration() {
     }
     let loader = FileSystemLoader::new(output_dir.clone());
 
-    let mut pipeline = Pipeline::new(tm, loader);
+    let mut pipeline = Pipeline::new(transformer_module, loader);
 
-    let csv_path = assets_path.clone().join("test_data.csv");
-    let excel_path = assets_path.clone().join("test_data.xlsx");
-
-    let mut alias_map_sex_col: HashMap<String, String> = HashMap::default();
-    alias_map_sex_col.insert("replace_me".to_string(), "male".to_string());
-
-    let mut data_sources = [
-        DataSource::Csv(CSVDataSource::new(
-            csv_path,
-            None,
-            TableContext::new(
-                "CSVTable".to_string(),
-                vec![
-                    SeriesContext::new(
-                        Identifier::Regex("patient_id".to_string()),
-                        Default::default(),
-                        Context::SubjectId,
-                        None,
-                        None,
-                        vec![],
-                    ),
-                    SeriesContext::new(
-                        Identifier::Regex("SEX".to_string()),
-                        Default::default(),
-                        Context::SubjectSex,
-                        None,
-                        Some(AliasMap::ToString(alias_map_sex_col)),
-                        vec![],
-                    ),
-                ],
-            ),
-            ExtractionConfig::new("CSVTable".to_string(), true, false),
-        )),
-        DataSource::Excel(ExcelDatasource::new(
-            excel_path,
-            vec![TableContext::new(
-                "HPOLabels".to_string(),
-                vec![
-                    SeriesContext::new(
-                        Identifier::Regex("0".to_string()),
-                        Default::default(),
-                        Context::SubjectId,
-                        None,
-                        None,
-                        vec![Identifier::Regex("4".to_string())],
-                    ),
-                    SeriesContext::new(
-                        Identifier::Regex("1".to_string()),
-                        Default::default(),
-                        Context::HpoLabel,
-                        None,
-                        None,
-                        vec![],
-                    ),
-                    SeriesContext::new(
-                        Identifier::Regex("4".to_string()),
-                        Default::default(),
-                        Context::OnsetAge,
-                        None,
-                        None,
-                        vec![],
-                    ),
-                ],
-            )],
-            vec![ExtractionConfig::new("HPOLabels".to_string(), false, true)],
-        )),
-    ];
+    //run the pipeline on the data sources
     let res = pipeline.run(&mut data_sources);
 
+    //check that everything was as expected
     assert!(res.is_ok());
 
     let expected_phenopackets_files =
         fs::read_dir(assets_path.join("integration_test_expected_phenopackets")).unwrap();
 
     let mut expected_phenopackets: HashMap<String, Phenopacket> = HashMap::new();
-    for pp_file in expected_phenopackets_files {
-        let data = fs::read_to_string(pp_file.unwrap().path()).unwrap();
-        let pp: Phenopacket = serde_json::from_str(&data).unwrap();
-        expected_phenopackets.insert(pp.id.clone(), pp);
+    for expected_pp_file in expected_phenopackets_files {
+        let data = fs::read_to_string(expected_pp_file.unwrap().path()).unwrap();
+        let expected_pp: Phenopacket = serde_json::from_str(&data).unwrap();
+        expected_phenopackets.insert(expected_pp.id.clone(), expected_pp);
     }
 
-    for pp_file_dir in fs::read_dir(output_dir).unwrap() {
-        let data = fs::read_to_string(pp_file_dir.unwrap().path()).unwrap();
-        let pp: Phenopacket = serde_json::from_str(&data).unwrap();
-        let pp_id = pp.id.clone();
-        assert_eq!(pp, expected_phenopackets.get(&pp_id).unwrap().clone());
+    for extracted_pp_file in fs::read_dir(output_dir).unwrap() {
+        let data = fs::read_to_string(extracted_pp_file.unwrap().path()).unwrap();
+        let extracted_pp: Phenopacket = serde_json::from_str(&data).unwrap();
+        let extracted_pp_id = extracted_pp.id.clone();
+        assert_eq!(extracted_pp, expected_phenopackets.get(&extracted_pp_id).unwrap().clone());
     }
 }
