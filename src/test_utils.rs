aeb71500717e9f78d2cf9e57fe8de019db712018
rouvenreuter <rouven.reuter91@gmail.com>
2025-10-01 12:00:42 +0200

    Keep skip_in_ci macro

--- src/test_utils.rs
+++ src/test_utils.rs
@@ -11,3 +11,19 @@ pub(crate) static HPO: Lazy<Arc<FullCsrOntology>> = Lazy::new(|| {
     let path = hpo_registry.register("v2025-09-01").unwrap();
     init_ontolius(path).unwrap()
 });
+
+#[macro_export]
+macro_rules! skip_in_ci {
+    ($test_name:expr) => {
+        if std::env::var("CI").is_ok() {
+            println!("Skipping {} in CI environment", $test_name);
+            return;
+        }
+    };
+    () => {
+        if std::env::var("CI").is_ok() {
+            println!("Skipping {} in CI environment", module_path!());
+            return;
+        }
+    };
+}
