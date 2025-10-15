dc0b76120193e16c2913f32b206b36305de159be
SmartMonkey <rouven.reuter91@gmail.com>
2025-10-15 11:13:34 +0200

    scoping

--- src/transform/mod.rs
+++ src/transform/mod.rs
@@ -3,7 +3,8 @@ pub use collector::Collector;
 pub mod error;
 pub mod phenopacket_builder;
 pub use phenopacket_builder::PhenopacketBuilder;
-mod cached_resource_resolver;
+pub(crate) mod cached_resource_resolver;
+
 mod phenopacket_linter;
 pub mod strategies;
 pub mod traits;
