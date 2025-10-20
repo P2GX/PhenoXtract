8d79c0a72c3ba7363673d66fcd0ef8e7cb2508c7
psnairne <patricknairne@hotmail.co.uk>
2025-10-20 14:51:58 +0200

    mod

--- src/transform/strategies/mod.rs
+++ src/transform/strategies/mod.rs
@@ -1,7 +1,9 @@
 pub mod alias_map;
 pub use alias_map::AliasMapStrategy;
 pub mod mapping;
-mod multi_hpo_col_expansion;
 pub use mapping::MappingStrategy;
 pub mod synonyms_to_primary_terms;
 pub use synonyms_to_primary_terms::SynonymsToPrimaryTermsStrategy;
+
+pub mod multi_hpo_col_expansion;
+pub use multi_hpo_col_expansion::MultiHPOColExpansionStrategy;
