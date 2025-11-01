5f2c8bbe859626c04ec969a60a4c5fb01fb9d353
SmartMonkey <rouven.reuter91@gmail.com>
2025-11-01 17:36:37 +0100

    Formatting

--- tests/usidnet_test.rs
+++ tests/usidnet_test.rs
@@ -1,18 +1,27 @@
 use std::{collections::HashMap, env::home_dir, fs, path::PathBuf};
 
-use phenoxtract::{Pipeline, config::table_context::{Context, Identifier, SeriesContext, TableContext}, extract::{DataSource, ExcelDatasource, extraction_config::ExtractionConfig}, load::FileSystemLoader, ontology::{CachedOntologyFactory, OntologyRef, traits::HasPrefixId}, transform::{Collector, PhenopacketBuilder, TransformerModule, strategies::{AliasMapStrategy, MappingStrategy, MultiHPOColExpansionStrategy, OntologyNormaliserStrategy}, traits::Strategy}};
+use phenoxtract::{
+    Pipeline,
+    config::table_context::{Context, Identifier, SeriesContext, TableContext},
+    extract::{DataSource, ExcelDatasource, extraction_config::ExtractionConfig},
+    load::FileSystemLoader,
+    ontology::{CachedOntologyFactory, OntologyRef, traits::HasPrefixId},
+    transform::{
+        Collector, PhenopacketBuilder, TransformerModule,
+        strategies::{
+            AliasMapStrategy, MappingStrategy, MultiHPOColExpansionStrategy,
+            OntologyNormaliserStrategy,
+        },
+        traits::Strategy,
+    },
+};
 use rstest::{fixture, rstest};
-
-
-
-
 
 #[fixture]
 fn usidnet_path() -> PathBuf {
     let homedir = home_dir().unwrap();
     homedir.join("data/usidnet/old_registry_data.xlsx")
 }
-
 
 #[fixture]
 fn cohort_sheet_context() -> TableContext {
@@ -26,17 +35,8 @@ fn cohort_sheet_context() -> TableContext {
     )
 }
 
-
-
-
-
-
 #[rstest]
-fn test_cohort_import(
-    usidnet_path: PathBuf,
-    cohort_sheet_context: TableContext
-) { 
-
+fn test_cohort_import(usidnet_path: PathBuf, cohort_sheet_context: TableContext) {
     let cohort_name = "USID-Net";
 
     let mut onto_factory = CachedOntologyFactory::default();
@@ -45,17 +45,13 @@ fn test_cohort_import(
         .build_bidict(&OntologyRef::hp(Some("2025-09-01".to_string())), None)
         .unwrap();
 
-    let mut data_sources = [   
-        DataSource::Excel(ExcelDatasource::new(
-            usidnet_path,
-            vec![cohort_sheet_context],
-            vec![
-                ExtractionConfig::new("Cohort".to_string(), true, true),
-            ],
-        )),
-    ]; 
+    let mut data_sources = [DataSource::Excel(ExcelDatasource::new(
+        usidnet_path,
+        vec![cohort_sheet_context],
+        vec![ExtractionConfig::new("Cohort".to_string(), true, true)],
+    ))];
 
-     //Configure strategies (a.k.a. transformations)
+    //Configure strategies (a.k.a. transformations)
     let strategies: Vec<Box<dyn Strategy>> = vec![
         Box::new(AliasMapStrategy),
         Box::new(OntologyNormaliserStrategy::new(
@@ -64,9 +60,9 @@ fn test_cohort_import(
         )),
         Box::new(MappingStrategy::default_sex_mapping_strategy()),
         Box::new(MultiHPOColExpansionStrategy),
-    ];   
+    ];
 
-      let phenopacket_builder = PhenopacketBuilder::new(HashMap::from_iter([(
+    let phenopacket_builder = PhenopacketBuilder::new(HashMap::from_iter([(
         hpo_dict.ontology.prefix_id().to_string(),
         hpo_dict,
     )]));
@@ -75,8 +71,12 @@ fn test_cohort_import(
         strategies,
         Collector::new(phenopacket_builder, cohort_name.to_owned()),
     );
-    let assets_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
-        .join(PathBuf::from(file!()).parent().unwrap().join("usidnet_assets"));
+    let assets_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join(
+        PathBuf::from(file!())
+            .parent()
+            .unwrap()
+            .join("usidnet_assets"),
+    );
     let output_dir = assets_path.join("do_not_push");
     if !output_dir.exists() {
         fs::create_dir_all(&output_dir).unwrap();
@@ -89,4 +89,3 @@ fn test_cohort_import(
     let res = pipeline.run(&mut data_sources);
     assert!(res.is_ok());
 }
-
