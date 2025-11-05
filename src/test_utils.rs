140bc0b2a43b073452cc81cc12b99f33ca5a8cf8
SmartMonkey <rouven.reuter91@gmail.com>
2025-11-05 13:18:02 +0100

    Fix cache lock

--- src/test_utils.rs
+++ src/test_utils.rs
@@ -1,8 +1,8 @@
 #![allow(unused)]
 
-use crate::ontology::CachedOntologyFactory;
 use crate::ontology::ontology_bidict::OntologyBiDict;
 use crate::ontology::resource_references::OntologyRef;
+use crate::ontology::{CachedOntologyFactory, HGNCClient};
 use once_cell::sync::Lazy;
 use ontolius::ontology::csr::FullCsrOntology;
 use phenopackets::schema::v1::core::Individual;
@@ -12,9 +12,13 @@ use phenopackets::schema::v2::core::{
 };
 use pretty_assertions::assert_eq;
 use prost_types::Timestamp;
+use ratelimit::Ratelimiter;
 use std::collections::{HashMap, HashSet};
 use std::fmt::Debug;
+use std::path::{Path, PathBuf};
 use std::sync::{Arc, Mutex};
+use std::time::Duration;
+use tempfile::TempDir;
 
 pub(crate) static ONTOLOGY_FACTORY: Lazy<Arc<Mutex<CachedOntologyFactory>>> =
     Lazy::new(|| Arc::new(Mutex::new(CachedOntologyFactory::default())));
@@ -61,6 +65,8 @@ pub(crate) static GENO_REF: Lazy<OntologyRef> =
     Lazy::new(|| OntologyRef::geno_with_version("2025-07-25"));
 pub(crate) static MONDO_REF: Lazy<OntologyRef> =
     Lazy::new(|| OntologyRef::mondo_with_version("2025-10-07"));
+use crate::ontology::traits::HasPrefixId;
+use crate::transform::PhenopacketBuilder;
 use validator::ValidateRequired;
 
 pub(crate) static HPO: Lazy<Arc<FullCsrOntology>> = Lazy::new(|| {
@@ -103,4 +109,46 @@ pub(crate) fn assert_phenopackets(actual: &mut Phenopacket, expected: &mut Pheno
         meta.created = None;
     }
     assert_eq!(actual, expected);
+}
+
+fn build_test_dicts() -> HashMap<String, Arc<OntologyBiDict>> {
+    let hpo_dict = ONTOLOGY_FACTORY
+        .lock()
+        .unwrap_or_else(|poisoned| poisoned.into_inner())
+        .build_bidict(&HPO_REF.clone(), None)
+        .unwrap();
+
+    let geno_dict = ONTOLOGY_FACTORY
+        .lock()
+        .unwrap_or_else(|poisoned| poisoned.into_inner())
+        .build_bidict(&GENO_REF.clone(), None)
+        .unwrap();
+
+    HashMap::from_iter(vec![
+        (hpo_dict.ontology.prefix_id().to_string(), hpo_dict),
+        (
+            MONDO_BIDICT.ontology.prefix_id().to_string(),
+            MONDO_BIDICT.clone(),
+        ),
+        (geno_dict.ontology.prefix_id().to_string(), geno_dict),
+    ])
+}
+
+pub(crate) fn build_hgnc_test_client(temp_dir: &Path) -> HGNCClient {
+    let rate_limiter = Ratelimiter::builder(10, Duration::from_secs(1))
+        .max_tokens(10)
+        .build()
+        .expect("Building rate limiter failed");
+
+    HGNCClient::new(
+        rate_limiter,
+        temp_dir.to_path_buf().join("hgnc_test_cache"),
+        "https://rest.genenames.org/".to_string(),
+    )
+    .unwrap()
+}
+
+pub(crate) fn build_test_phenopacket_builder(temp_dir: &Path) -> PhenopacketBuilder {
+    let hgnc_client = build_hgnc_test_client(temp_dir);
+    PhenopacketBuilder::new(build_test_dicts(), hgnc_client)
 }
