49ca6997435a53b476274d84f4c67c96e2fa5e31
psnairne <patricknairne@hotmail.co.uk>
2025-11-21 16:26:03 +0100

    rouven's suggestion

--- src/transform/transform_module.rs
+++ src/transform/transform_module.rs
@@ -40,7 +40,6 @@ impl TransformerModule {
 
         for table in &mut tables_refs {
             Self::trim_strings(table)?;
-            Self::empty_strings_to_nulls(table)?;
             Self::ensure_ints(table)?;
             Self::polars_dataframe_cast_ambivalent(table)?;
         }
@@ -63,40 +62,19 @@ impl TransformerModule {
 
         for col_name in string_col_names {
             let column = cdf.data().column(&col_name)?;
-            let trimmed_col = column.str()?.apply_mut(|s| s.trim());
-            cdf.builder()
-                .replace_column(&col_name, trimmed_col.into_series())?
-                .build()?;
-        }
-        Ok(())
-    }
-
-    fn empty_strings_to_nulls(
-        cdf: &mut ContextualizedDataFrame,
-    ) -> Result<(), DataProcessingError> {
-        let string_col_names: Vec<String> = cdf
-            .filter_columns()
-            .where_dtype(Filter::Is(&DataType::String))
-            .collect()
-            .iter()
-            .map(|col| col.name().to_string())
-            .collect();
-
-        for col_name in string_col_names {
-            let column = cdf.data().column(&col_name)?;
-            let nulled_col = column.str()?.apply(|opt_s| {
-                if let Some(s) = opt_s {
-                    if s.is_empty() {
+            let trimmed_col = column.str()?.apply(|s| match s {
+                None => None,
+                Some(s) => {
+                    let trimmed = s.trim();
+                    if trimmed.is_empty() {
                         None
                     } else {
-                        Some(Cow::Borrowed(s))
+                        Some(Cow::Borrowed(trimmed))
                     }
-                } else {
-                    None
                 }
             });
             cdf.builder()
-                .replace_column(&col_name, nulled_col.into_series())?
+                .replace_column(&col_name, trimmed_col.into_series())?
                 .build()?;
         }
         Ok(())
@@ -389,42 +367,6 @@ mod tests {
                 "int col" => &[1, 2, 3, 4, 5],
             ]
             .unwrap()
-        );
-    }
-
-    #[rstest]
-    fn test_empty_strings_to_nulls() {
-        let df = df![
-            "subject_id" => ["P001", "P002", "P003", "P004", "P005"],
-            "string_col" => &["", "hello", "", "blah", "  "],
-            "int_col" => &[1, 2, 3, 4, 5],
-        ]
-        .unwrap();
-        let mut cdf = ContextualizedDataFrame::new(
-            TableContext::new(
-                "table".to_string(),
-                vec![
-                    SeriesContext::default()
-                        .with_identifier(Identifier::Regex("subject_id".to_string()))
-                        .with_data_context(Context::SubjectId),
-                    SeriesContext::default()
-                        .with_identifier(Identifier::Regex("string_col".to_string())),
-                    SeriesContext::default()
-                        .with_identifier(Identifier::Regex("int_col".to_string())),
-                ],
-            ),
-            df,
-        );
-
-        TransformerModule::empty_strings_to_nulls(&mut cdf).unwrap();
-
-        assert_eq!(
-            cdf.data(),
-            &df!["subject_id" => ["P001", "P002", "P003", "P004", "P005"],
-                "string_col" => &[AnyValue::Null, AnyValue::String("hello"), AnyValue::Null, AnyValue::String("blah"), AnyValue::String("  ")],
-                "int col" => &[1, 2, 3, 4, 5],
-            ]
-                .unwrap()
         );
     }
 }
