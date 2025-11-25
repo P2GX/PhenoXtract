8e5a664f4c463b157008280ea71854463008bb98
psnairne <patricknairne@hotmail.co.uk>
2025-11-25 13:32:37 +0100

    formatting

--- src/extract/contextualized_data_frame.rs
+++ src/extract/contextualized_data_frame.rs
@@ -1,10 +1,10 @@
-use crate::validation::contextualised_dataframe_validation::validate_full_subject_id_column;
 use crate::config::context::Context;
 use crate::config::table_context::{Identifier, SeriesContext, TableContext};
 use crate::extract::contextualized_dataframe_filters::{ColumnFilter, Filter, SeriesContextFilter};
 use crate::transform::error::StrategyError;
 use crate::validation::cdf_checks::check_orphaned_columns;
 use crate::validation::contextualised_dataframe_validation::validate_dangling_sc;
+use crate::validation::contextualised_dataframe_validation::validate_full_subject_id_column;
 use crate::validation::contextualised_dataframe_validation::{
     validate_one_context_per_column, validate_single_subject_id_column,
 };
