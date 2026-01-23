354c934a79b27f2179c829db9ea8176f0ae4929b
SmartMonkey <rouven.reuter91@gmail.com>
2026-01-23 09:51:00 +0100

    post merge

--- src/ontology/traits.rs
+++ src/ontology/traits.rs
@@ -2,7 +2,6 @@ use crate::ontology::error::BiDictError;
 use crate::ontology::resource_references::ResourceRef;
 use std::fmt::Debug;
 
-
 pub trait HasPrefixId {
     fn prefix_id(&self) -> &str;
 }
