6689dc2cd773cb450db3ec3cfef64ce4046cbaf8
SmartMonkey <rouven.reuter91@gmail.com>
2026-01-27 12:24:44 +0100

    FileSystemLoader: add remove_default_survival_time()
    Sadly, the protobuf schema does not define survivalTimeInDays as optional, hence it defaults to 0.
    To circumvent that, we remove all survivalTimeInDays==0.

--- src/load/error.rs
+++ src/load/error.rs
@@ -2,8 +2,10 @@ use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum LoadError {
-    #[error("IO Error: {0}")]
-    IO(#[from] std::io::Error),
-    #[error("SerdeJson Error: {0}")]
-    SerdeJson(serde_json::Error),
+    #[error("Can not find storage: {reason}")]
+    NoStorage { reason: String },
+    #[error("Can not store Phenopacket with ID '{pp_id}', because: {reason}")]
+    CantStore { pp_id: String, reason: String },
+    #[error("Can not convert Phenopacket with ID '{pp_id}' into '{format}'")]
+    ConversionError { pp_id: String, format: String },
 }
