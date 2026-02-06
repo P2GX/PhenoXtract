23a3a80285441855c6cfd31744f4eb026629ec0a
SmartMonkey <rouven.reuter91@gmail.com>
2026-02-06 14:58:37 +0100

    Remove trait

--- src/caching/traits.rs
+++
@@ -1,7 +0,0 @@
-use crate::caching::error::CacheError;
-
-pub trait Caching<Key, Value> {
-    fn write(&mut self, key: &Key, value: &Value) -> Result<(), CacheError>;
-
-    fn read(&self, key: &Key) -> Result<&Value, CacheError>;
-}
