9b1b85afa2c2979cd5221ef3e7f2304db2cf7980
psnairne <patricknairne@hotmail.co.uk>
2026-02-10 21:41:07 +0100

    phenoxtract.run()

--- integration_tests/tests/i_data_test.rs
+++ integration_tests/tests/i_data_test.rs
@@ -1,17 +1,14 @@
 use dotenvy::dotenv;
-use phenoxtract::Pipeline;
-use phenoxtract::config::PhenoXtractConfig;
 use rstest::rstest;
 use std::path::PathBuf;
+use phenoxtract::phenoxtract::Phenoxtract;
 
 #[rstest]
 fn test_i_data() {
     dotenv().ok();
     let config_path = PathBuf::from(
-        "/Users/patrick/RustroverProjects/PhenoXtrackt/tests/configs/i_data_config.yaml",
+        "/Users/patrick/RustroverProjects/PhenoXtrackt/integration_tests/tests/configs/i_data_config.yaml",
     );
-    let phenoxtract_config = PhenoXtractConfig::try_from(config_path).unwrap();
-    let mut pipeline = Pipeline::try_from(phenoxtract_config.pipeline).unwrap();
-    let mut data_sources = phenoxtract_config.data_sources;
-    pipeline.run(&mut data_sources).unwrap();
+    let mut phenoxtract = Phenoxtract::try_from(config_path).unwrap();
+    phenoxtract.run().unwrap();
 }
