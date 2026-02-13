6e44f4444c650cde2df090edbe994516505fd35e
Adam Graefe <adam.graefe@charite.de>
2026-02-13 16:59:38 +0100

    kept on trying the pipeline for japan data with fix improvements of config yaml

--- integration_tests/tests/test_complete_keio.rs
+++ integration_tests/tests/test_complete_keio.rs
@@ -1,12 +1,15 @@
+use dotenvy::dotenv;
 use phenoxtract::config::{ConfigLoader, PhenoXtractConfig};
 use phenoxtract::phenoxtract::Phenoxtract;
 use std::path::PathBuf;
 
 #[test]
 fn test_pipeline_integration() {
+    dotenv().ok();
     let config: PhenoXtractConfig =
         ConfigLoader::load(PathBuf::from("/Users/adamgraefe/Documents/git/PhenoXtract/integration_tests/tests/assets/japan_config.yaml")).unwrap();
         let mut px = Phenoxtract::try_from(config).unwrap();
         px.run().unwrap();
+
 }
 
