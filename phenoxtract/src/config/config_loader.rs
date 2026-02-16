c2b150aa231642df07530000a85989e79df7460b
SmartMonkey <rouven.reuter91@gmail.com>
2026-02-16 14:29:51 +0100

    Add sub_building_blocks

--- phenoxtract/src/config/config_loader.rs
+++ phenoxtract/src/config/config_loader.rs
@@ -225,6 +225,7 @@ blah: "blahblah"
                                     output_data_type: OutputDataType::Float64,
                                 }),
                                 building_block_id: None,
+                                sub_blocks: vec![],
                             }],
                         },
                         ExcelSheetConfig {
@@ -250,6 +251,7 @@ blah: "blahblah"
                                     output_data_type: OutputDataType::Boolean,
                                 }),
                                 building_block_id: None,
+                                sub_blocks: vec![],
                             }],
                         },
                     ],
