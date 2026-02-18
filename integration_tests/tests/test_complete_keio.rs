2b082029fbfa7472349646671e79df92f49b8aff
Adam Graefe <adam.graefe@charite.de>
2026-02-18 18:55:47 +0100

    formatted

--- integration_tests/tests/test_complete_keio.rs
+++ integration_tests/tests/test_complete_keio.rs
@@ -6,10 +6,10 @@ use std::path::PathBuf;
 #[test]
 fn test_pipeline_integration() {
     dotenv().ok();
-    let config: PhenoXtractConfig =
-        ConfigLoader::load(PathBuf::from("/Users/adamgraefe/Documents/git/PhenoXtract/integration_tests/tests/japan_config.yaml")).unwrap();
-        let mut px = Phenoxtract::try_from(config).unwrap();
-        px.run().unwrap();
-
+    let config: PhenoXtractConfig = ConfigLoader::load(PathBuf::from(
+        "/Users/adamgraefe/Documents/git/PhenoXtract/integration_tests/tests/japan_config.yaml",
+    ))
+    .unwrap();
+    let mut px = Phenoxtract::try_from(config).unwrap();
+    px.run().unwrap();
 }
-
