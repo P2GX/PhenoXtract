62a790c4bbf519eb6d6432430c2ca8c8d64f3de3
SmartMonkey <rouven.reuter91@gmail.com>
2026-03-06 12:07:43 +0100

    Update config

--- integration_tests/tests/i_data_test.rs
+++ integration_tests/tests/i_data_test.rs
@@ -4,11 +4,13 @@ use rstest::rstest;
 use std::path::PathBuf;
 
 #[rstest]
-fn test_i_data() {
+fn test_i_data() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
     let config_path = PathBuf::from(
         "/Users/rouvenreuter/Documents/Projects/PhenoXtrackt/integration_tests/tests/configs/i_data_config.yaml",
     );
     let mut phenoxtract = Phenoxtract::try_from(config_path).unwrap();
-    phenoxtract.run().unwrap();
+    phenoxtract.run()?;
+
+    Ok(())
 }
