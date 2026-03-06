4b190fc620c29cbc51c9bcd6fa820ef6e2b5f6a6
SmartMonkey <rouven.reuter91@gmail.com>
2026-03-06 13:35:48 +0100

    use project dir

--- integration_tests/tests/i_data_test.rs
+++ integration_tests/tests/i_data_test.rs
@@ -6,9 +6,10 @@ use std::path::PathBuf;
 #[rstest]
 fn test_i_data() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
-    let config_path = PathBuf::from(
-        "/Users/rouvenreuter/Documents/Projects/PhenoXtrackt/integration_tests/tests/configs/i_data_config.yaml",
-    );
+    let root = env!("CARGO_MANIFEST_DIR");
+    let config_path = PathBuf::from(&format!("{}/tests/configs/i_data_config.yaml", root));
+
+    println!("config_path: {:?}", config_path);
     let mut phenoxtract = Phenoxtract::try_from(config_path).unwrap();
     phenoxtract.run()?;
 
