0036d94fa85069a2371f735df3f5e4a90c73f72b
lukas <lukas.ramlow@bih-charite.de>
2026-03-09 12:57:24 +0100

    Weird data not working

--- integration_tests/tests/i_data_test.rs
+++ integration_tests/tests/i_data_test.rs
@@ -7,7 +7,7 @@ use std::path::PathBuf;
 fn test_i_data() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
     let root = env!("CARGO_MANIFEST_DIR");
-    let config_path = PathBuf::from(&format!("{}/tests/configs/i_data_config.yaml", root));
+    let config_path = PathBuf::from("/home/lukas/soft/PhenoXtract/integration_tests/tests/configs/i_data_config.yaml");
 
     println!("config_path: {:?}", config_path);
     let mut phenoxtract = Phenoxtract::try_from(config_path).unwrap();
