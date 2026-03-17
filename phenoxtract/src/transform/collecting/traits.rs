3071d80ae6d05f1bf6ed2b229a6ff379cebabc0f
SmartMonkey <rouven.reuter91@gmail.com>
2026-03-17 17:24:13 +0100

    Documentation

--- phenoxtract/src/transform/collecting/traits.rs
+++ phenoxtract/src/transform/collecting/traits.rs
@@ -28,16 +28,6 @@ pub(crate) trait Getter {
 /// `Pluck` allows you to apply a function to the inner value of an `Option<T>`
 /// by reference, returning a new `Option<U>`. This is useful when you want to
 /// extract a sub-value from a type without consuming or cloning the outer `Option`.
-///
-/// # Example
-///
-/// ```rust
-/// let s: Option<String> = Some("hello world".to_string());
-///
-/// // Extract the length only if the string contains a space
-/// let len = s.pluck(|s| s.contains(' ').then(|| s.len()));
-/// assert_eq!(len, Some(11));
-/// ```
 pub(crate) trait Pluck<T> {
     /// Applies `f` to a reference of the inner value, returning the result.
     ///
