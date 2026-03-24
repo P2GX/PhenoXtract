65a1656746e55ad73ad7254fe37b04224c535e3f
SmartMonkey <rouven.reuter91@gmail.com>
2026-03-24 17:24:35 +0100

    Post merge

--- phenoxtract/src/transform/collecting/medical_actions/medical_procedure_collector.rs
+++ phenoxtract/src/transform/collecting/medical_actions/medical_procedure_collector.rs
@@ -2,15 +2,11 @@ use crate::config::context::Context;
 use crate::extract::ContextualizedDataFrame;
 
 use crate::extract::enums::Filter;
-use crate::transform::collecting::medical_actions::medical_action::MedicalActionData;
 use crate::transform::collecting::medical_actions::medical_procedure_data::ProcedureData;
 use crate::transform::collecting::traits::{Collect, GetRows, Pluck};
 use crate::transform::error::{CollectorError, GetterError};
 
 use crate::transform::collecting::medical_actions::medical_action_data::MedicalActionData;
-use crate::transform::collecting::medical_actions::procedure_data::ProcedureData;
-use crate::transform::collecting::traits::{Collect, GetRows, Pluck};
-use crate::transform::error::{CollectorError, GetterError};
 use crate::transform::traits::PhenopacketBuilding;
 use std::any::Any;
 
