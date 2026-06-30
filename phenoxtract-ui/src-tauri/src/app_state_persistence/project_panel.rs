f6c60abb6723f082e81689b7a3c1684c7b75d5a1
SmartMonkey <rouven.reuter91@gmail.com>
2026-06-30 17:31:14 +0200

    Add project panel struct

---
+++ phenoxtract-ui/src-tauri/src/app_state_persistence/project_panel.rs
@@ -0,0 +1,19 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct ProjectPanel {
+    pub name: String,
+    pub dir: String,
+    #[serde(rename = "squareColor")]
+    pub square_color: String,
+}
+
+impl ProjectPanel {
+    pub fn new(name: impl Into<String>, dir: impl Into<String>, color: impl Into<String>) -> Self {
+        Self {
+            name: name.into(),
+            dir: dir.into(),
+            square_color: color.into(),
+        }
+    }
+}
